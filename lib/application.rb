5ba8be8ba6da34cdfd64454ef6fa50d91402bf26
benilovj <benilov@gmail.com>
2011-10-03 17:35:33 +0200

    copied optional require of rubygems

--- lib/application.rb
+++ lib/application.rb
@@ -1,5 +1,9 @@
-require 'rubygems' # disable this for a deployed application
-require 'hotcocoa'
+begin
+ require 'hotcocoa'
+rescue LoadError
+  require 'rubygems'
+  require 'hotcocoa'
+end
 
 require 'lib/document'
 
