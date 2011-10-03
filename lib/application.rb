79edc1fee5239847dbae68516ad1c19a5afd16ff
benilovj <benilov@gmail.com>
2011-10-03 18:00:52 +0200

    moved hotcocoa mappings for NSTrackingArea to the hotcocoa gem

--- lib/application.rb
+++ lib/application.rb
@@ -37,31 +37,6 @@ module PngImages
   end
 end
 
-HotCocoa::Mappings.map tracking_area: NSTrackingArea do
-  defaults rect: CGRectZero
-
-  constant :options, {
-    mouse_entered_and_exited: NSTrackingMouseEnteredAndExited,
-    mouse_moved:              NSTrackingMouseMoved,
-    cursor_update:            NSTrackingCursorUpdate,
-    active_in_key_window:     NSTrackingActiveInKeyWindow,
-    active_first_responder:   NSTrackingActiveWhenFirstResponder,
-    active_in_active_app:     NSTrackingActiveInActiveApp,
-    active_always:            NSTrackingActiveAlways,
-    assume_inside:            NSTrackingAssumeInside,
-    in_visible_rectangle:     NSTrackingInVisibleRect,
-    enabled_during_drag:      NSTrackingEnabledDuringMouseDrag
-  }
-  
-  def init_with_options tracking_area, options
-    rect = options.delete(:rect)
-    tracking_area.initWithRect rect,
-                      options: options.delete(:options),
-                        owner: options.delete(:owner),
-                     userInfo: nil
-  end
-end
-
 class RotationEventResponder
   attr_writer :delegate
   
