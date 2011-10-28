8ca18239dde16a144c739ebd0e7f0675ac9f725d
Jake Benilov <benilov@gmail.com>
2011-10-28 18:55:05 +0200

    organised code a bit better

--- lib/appkit/rotatable_image_controller.rb
+++ lib/appkit/rotatable_image_controller.rb
@@ -1,7 +1,57 @@
 require 'lib/core_extensions/nspoint'
 
+class RotationResponder
+  def initialize(controller, view)
+    @controller = controller
+    @view = view
+  end
+
+  def mouseDown(relative_location)
+    @initial_rotation = @view.frameCenterRotation
+    @initial_angle = (relative_location - @view.center).to_radial.degrees
+  end
+
+  def mouseDragged(relative_location)
+    delta = relative_location - @view.center
+    @view.setFrameCenterRotation(@initial_rotation + delta.to_radial.degrees - @initial_angle)
+  end
+
+  def mouseUp(relative_location)
+    delta = relative_location - @view.center
+    @controller.rotation = @initial_rotation + delta.to_radial.degrees - @initial_angle
+  end
+end
+
+class DraggingResponder
+  def initialize(controller, view)
+    @controller = controller
+    @view = view
+  end
+
+  def mouseDown(relative_location)
+    @initial_location = relative_location
+    @initial_origin = @view.frame.origin
+  end
+
+  def mouseDragged(relative_location)
+    delta = relative_location- @initial_location
+    @view.setFrameOrigin @initial_origin + delta
+  end
+
+  def mouseUp(relative_location)
+    delta = relative_location - @initial_location
+    @controller.shift_by(delta)
+  end
+end
+
 class RotatableImageController
   attr_accessor :view
+  
+  def view=(new_view)
+    @view = new_view
+    @rotation_responder = RotationResponder.new(self, new_view)
+    @dragging_responder = DraggingResponder.new(self, new_view)
+  end
   
   def shift_by(delta)
     @floor_plan.position += delta
@@ -28,51 +78,36 @@ class RotatableImageController
   end
   
   def mouseDown(event)
+    @rotation_occuring = @cursor_over_grip
     @mouse_pressed = true
     update_cursor
     view.select
 
-    @rotation_occuring = @cursor_over_grip
-
-    if @rotation_occuring
-      @initial_rotation = view.frameCenterRotation
-      @initial_angle = (view.relative_location_of(event) - view.center).to_radial.degrees
-    else
-      @initial_location = view.relative_location_of(event)
-      @initial_origin = view.frame.origin
-    end
+    appropriate_responder.mouseDown(@view.relative_location_of(event))
   end
   
   def mouseDragged(event)
-    if @rotation_occuring
-      delta = view.relative_location_of(event) - view.center
-      view.setFrameCenterRotation(@initial_rotation + delta.to_radial.degrees - @initial_angle)
-    else
-      delta = view.relative_location_of(event) - @initial_location
-      view.setFrameOrigin @initial_origin + delta
-    end
+    appropriate_responder.mouseDragged(@view.relative_location_of(event))
   end
 
   def mouseUp(event)
     @mouse_pressed = false
     update_cursor
 
-    if @rotation_occuring
-      delta = view.relative_location_of(event) - view.center
-      self.rotation = @initial_rotation + delta.to_radial.degrees - @initial_angle
-    else
-      delta = view.relative_location_of(event) - @initial_location
-      shift_by(delta)
-    end
+    appropriate_responder.mouseUp(@view.relative_location_of(event))
 
     @rotation_occuring = false
   end
 
   protected
+  def appropriate_responder
+    @rotation_occuring ? @rotation_responder : @dragging_responder
+  end
+  
   def update_cursor
     return if NSCursor.currentCursor.nil?
     case
-    when (@cursor_over_grip and @mouse_pressed) then rotate_cursor.set
+    when @rotation_occuring then rotate_cursor.set
     when (@cursor_over_grip and not @mouse_pressed) then NSCursor.openHandCursor.set
     else NSCursor.arrowCursor.set
     end
