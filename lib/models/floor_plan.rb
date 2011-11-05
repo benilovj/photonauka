0a756225a1393380a90476a655dd13efdcd4f594
Jake Benilov <benilov@gmail.com>
2011-11-05 19:54:22 +0100

    support for several devices (fixes #28)

--- lib/models/floor_plan.rb
+++ lib/models/floor_plan.rb
@@ -54,11 +54,11 @@ class FloorPlan
   attr_reader :devices
   
   def undo_manager=(new_undo_manager)
-    @devices.first.undo_manager = new_undo_manager
+    @devices.each {|device| device.undo_manager = new_undo_manager}
   end
 
   def initialize
-    @devices = [Device.new(NSPoint.new(100,100), 45)]
+    @devices = [Device.new(NSPoint.new(100,100), 45), Device.new(NSPoint.new(300,45), 145)]
     @devices.each do |device|
       NSNotificationCenter.defaultCenter.addObserver self, selector:'refresh', name:DEVICE_CHANGE_NOTIFICATION, object:device
     end
