27de0eb244601e39bbf8051ff234e14f00eb1ec5
Jake Benilov <benilov@gmail.com>
2011-11-05 20:30:51 +0100

    fixed failing specs

--- spec/appkit/rotatable_image_view_spec.rb
+++ spec/appkit/rotatable_image_view_spec.rb
@@ -7,10 +7,10 @@ require 'appkit/rotatable_image_controller'
 describe RotatableImageView do
   include HotCocoa
   before do
-    @floor_plan = Struct.new(:position, :rotation).new(NSPoint.new(0, 0), 0)
+    @device = Struct.new(:position, :rotation).new(NSPoint.new(0, 0), 0)
     
     @view = rotatable_image_view(frame: [-10, -5, 20, 10])
-    @view.delegate.floor_plan = @floor_plan
+    @view.delegate.device = @device
     view(frame: [0, 0, 100, 100]) do |v|
       v << @view
     end
@@ -25,14 +25,14 @@ describe RotatableImageView do
     mid_drag = mock("event", :locationInWindow => NSPoint.new(5, 1))
     @view.mouseDragged(mid_drag)
     @view.frame.origin.should be_near_point(-6, -5)
-    @floor_plan.position.should be_near_point(0, 0)
+    @device.position.should be_near_point(0, 0)
 
     ending_drag = mock("event", :locationInWindow => NSPoint.new(6, 2))
     @view.mouseDragged(ending_drag)
     @view.mouseUp(ending_drag)
     @view.frame.origin.should be_near_point(-5, -4)
-    @floor_plan.position.should be_near_point(5, 1)
-    @floor_plan.rotation.should == 0
+    @device.position.should be_near_point(5, 1)
+    @device.rotation.should == 0
   end
   
   it "should rotate" do
@@ -46,14 +46,14 @@ describe RotatableImageView do
 
     mid_drag = mock("event", :locationInWindow => NSPoint.new(-5, 0))
     @view.mouseDragged(mid_drag)
-    @floor_plan.position.should be_near_point(0, 0)
-    @floor_plan.rotation.should == 0
+    @device.position.should be_near_point(0, 0)
+    @device.rotation.should == 0
 
     ending_drag = mock("event", :locationInWindow => NSPoint.new(10, 5))
     @view.mouseDragged(ending_drag)
     @view.mouseUp(ending_drag)
     @view.frame.origin.should be_near_point(10, 5)
-    @floor_plan.position.should be_near_point(0, 0)
-    @floor_plan.rotation.should be_within(0.01).of(180.0)
+    @device.position.should be_near_point(0, 0)
+    @device.rotation.should be_within(0.01).of(180.0)
   end
 end
